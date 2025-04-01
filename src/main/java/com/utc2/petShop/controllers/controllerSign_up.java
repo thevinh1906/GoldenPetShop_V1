ace57f9e7ced157522452ce343470af8ace5b215
trangkimdat2005 <trangkimdatst2005@gmail.com>
2025-04-02 03:36:38 +0700

    co sua trong file sign up

--- src/main/java/com/utc2/petShop/controllers/controllerSign_up.java
+++ src/main/java/com/utc2/petShop/controllers/controllerSign_up.java
@@ -37,35 +37,53 @@ public class controllerSign_up implements Initializable {
     private Label labelAnnouncement;
 
     @FXML
+    private Label labelPasswordAnnouncement;
+
+    String regex = "^(?=.*[a-zA-Z])(?=.*\\d).{8,}$";
+
+    @FXML
     void actionLogin(ActionEvent event) throws IOException {
         scenes.switchScene("sampleSign_in", "Sign in", "applicationSign_In", false);
     }
 
     @FXML
     void actionSignUp(ActionEvent event) throws IOException {
-        if (textPassword.getText().equals(textConfirmPassword.getText())) {
+        if(textPassword.getText().matches(regex)) {
+            if (textPassword.getText().equals(textConfirmPassword.getText())) {
 
-            String username;
-            username = textUsername.getText();   //lấy dữ liệu username
+                String username;
+                username = textUsername.getText();   //lấy dữ liệu username
 
-            String password;
-            password = textPassword.getText(); //lấy dữ liệu password
+                String password;
+                password = textPassword.getText(); //lấy dữ liệu password
 
-            scenes.switchScene("sampleInsertUser", "Golden Pet Shop", "applicationInsertUser", false);
-        } else {
-            labelAnnouncement.setVisible(true);
+                scenes.switchScene("sampleInsertUser", "Golden Pet Shop", "applicationInsertUser", false);
+            } else {
+                labelAnnouncement.setVisible(true);
+            }
+        }
+        else {
+            labelPasswordAnnouncement.setVisible(true);
         }
 
     }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        labelPasswordAnnouncement.setVisible(false);
         labelAnnouncement.setVisible(false);
         buttonSignUp.setDisable(true);
         textUsername.textProperty().addListener((observable, oldValue, newValue) -> updateSignUpButtonState());
         textPassword.textProperty().addListener((observable, oldValue, newValue) -> updateSignUpButtonState());
         textConfirmPassword.textProperty().addListener((observable, oldValue, newValue) -> updateSignUpButtonState());
         Platform.runLater(() -> textUsername.requestFocus());
+
+        textPassword.textProperty().addListener((observable, oldValue, newValue) -> {
+            if (newValue.matches(regex)) {
+                labelPasswordAnnouncement.setVisible(false);
+            }
+            else labelPasswordAnnouncement.setVisible(true);
+        });
 
         textUsername.setOnKeyReleased((event) -> {
             if (event.getCode() == KeyCode.ENTER) {
